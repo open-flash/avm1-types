c9cc0918023dd7576449d4a1aa7194b537dc51dd
Charles Samborski <demurgos@demurgos.net>
2019-03-05 18:28:52 +0100

    [ts] Use CfgAction in CfgBlock (instead of raw Action)

--- ts/src/lib/cfg-block.ts
+++ ts/src/lib/cfg-block.ts
@@ -1,19 +1,19 @@
 import { CaseStyle } from "kryo/case-style";
 import { ArrayType } from "kryo/types/array";
 import { DocumentIoType, DocumentType } from "kryo/types/document";
-import { $Action, Action } from "./action";
+import { $CfgAction, CfgAction } from "./cfg-action";
 import { $CfgLabel, CfgLabel } from "./cfg-label";
 
 export interface CfgBlock {
   readonly label: CfgLabel;
-  readonly actions: ReadonlyArray<Action>;
+  readonly actions: ReadonlyArray<CfgAction>;
   readonly next?: CfgLabel;
 }
 
 export const $Block: DocumentIoType<CfgBlock> = new DocumentType<CfgBlock>(() => ({
   properties: {
     label: {type: $CfgLabel},
-    actions: {type: new ArrayType({itemType: $Action, maxLength: Infinity})},
+    actions: {type: new ArrayType({itemType: $CfgAction, maxLength: Infinity})},
     next: {type: $CfgLabel, optional: true},
   },
   changeCase: CaseStyle.SnakeCase,
